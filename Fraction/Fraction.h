6d5da39d2d1baf85095022c5c26b0425dfd6b950
Susan Valenzuela <susanvalenzuela70@gmail.com>
2025-04-28 22:56:32 -0600

    bug fixes with the gcd

--- Fraction/Fraction.h
+++ Fraction/Fraction.h
@@ -1,1 +1,79 @@
-#pragma once
+#include <iostream>
+
+namespace mathlib {
+
+    template <typename T>
+    class Fraction {
+    private:
+        T numerator;
+        T denominator;
+
+        // GCD implementation 
+        // gcd not apart of the std?
+        T gcd(T a, T b) const {
+            while (b != 0) {
+                T temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+    public:
+        // Constructor
+        Fraction(T num = 0, T den = 1) : numerator(num), denominator(den) {
+            if (denominator == 0) throw std::invalid_argument("Denominator cannot be zero.");
+        }
+
+        // Simplify the fraction
+        void Simplify() {
+            T gcd_value = gcd(numerator, denominator);
+            numerator /= gcd_value;
+            denominator /= gcd_value;
+        }
+
+        // Convert to double
+        double toDouble() const {
+            return static_cast<double>(numerator) / denominator;
+        }
+
+        // Equality operator (==)
+        bool operator==(const Fraction<T>& other) const {
+            return numerator * other.denominator == denominator * other.numerator;
+        }
+
+        // Greater-than operator (>)
+        bool operator>(const Fraction<T>& other) const {
+            return numerator * other.denominator > denominator * other.numerator;
+        }
+
+        // Addition operator (+)
+        Fraction<T> operator+(const Fraction<T>& other) const {
+            T common_denominator = denominator * other.denominator;
+            T new_numerator = numerator * other.denominator + other.numerator * denominator;
+            return Fraction<T>(new_numerator, common_denominator);
+        }
+
+        // Multiplication operator (*)
+        Fraction<T> operator*(const Fraction<T>& other) const {
+            T new_numerator = numerator * other.numerator;
+            T new_denominator = denominator * other.denominator;
+            return Fraction<T>(new_numerator, new_denominator);
+        }
+
+        // Output stream operator (<<)
+        friend std::ostream& operator<<(std::ostream& os, const Fraction<T>& frac) {
+            os << frac.numerator << "/" << frac.denominator;
+            return os;
+        }
+
+        // Input stream operator (>>)
+        friend std::istream& operator>>(std::istream& is, Fraction<T>& frac) {
+            char slash;
+            is >> frac.numerator >> slash >> frac.denominator;
+            if (frac.denominator == 0) is.setstate(std::ios::failbit);
+            return is;
+        }
+    };
+
+} 
--- Fraction/Main.cpp
+++ Fraction/Main.cpp
@@ -1,25 +1,28 @@
-
 #include <iostream>
+#include "Fraction.h"
 
-int main()
-{
-    Fraction fraction1{ 15, 5 };
+int main() {
+    mathlib::Fraction<int> fraction1{ 15, 5 };
     std::cout << fraction1 << std::endl;
 
-    Fraction fraction2{ 15, 6 };
+    mathlib::Fraction<int> fraction2{ 15, 6 };
     fraction2.Simplify();
     std::cout << fraction2 << std::endl;
 
     std::cout << fraction1 << " == " << fraction2 << " result: " << (fraction1 == fraction2) << std::endl;
+
     std::cout << fraction1 << " > " << fraction2 << " result: " << (fraction1 > fraction2) << std::endl;
 
     std::cout << fraction1 << " + " << fraction2 << " result: " << (fraction1 + fraction2) << std::endl;
+
     std::cout << fraction1 << " * " << fraction2 << " result: " << (fraction1 * fraction2) << std::endl;
 
-    Fraction fraction3;
-    std::cout << "input fraction (numerator - denominator): \n";
+    mathlib::Fraction<int> fraction3;
+    std::cout << "Enter a fraction (format: numerator/denominator): \n";
     std::cin >> fraction3;
+
     std::cout << fraction3 << std::endl;
     std::cout << fraction3.toDouble() << std::endl;
-}
 
+    return 0;
+}
